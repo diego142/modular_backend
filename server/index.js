933d4358e7962be1f50bbad95e734fe9fa6c94e5
FRIAS ACOSTA DIEGO <mega-311@hotmail.com>
2020-08-26 19:02:56 -0500

    create proyect

---
+++ server/index.js
@@ -0,0 +1,19 @@
+const express = require('express');
+const cors = require('cors');
+const app = express();
+
+// Settings
+app.set('port', process.env.PORT || 3000);
+
+// Middlewares
+app.use(express.json());
+app.use(cors());
+app.use(express.urlencoded({extended: false}));
+
+// Routes
+//app.use(require('./routes/index'));
+
+// Starting server
+app.listen(app.get('port'), () => {
+    console.log('server on port: ' + app.get('port'));
+});
